66ba8aa949f3bc88e84cd5f8ecbffc444d355730
Filip <f@filip.world>
2023-11-20 15:43:48 -0500

    stats for friend

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module testin
 go 1.21.4
 
 require (
+	gonum.org/v1/gonum v0.14.0
+	gonum.org/v1/plot v0.14.0
+)
+
+require (
 	git.sr.ht/~sbinet/gg v0.5.0 // indirect
 	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
 	github.com/campoy/embedmd v1.0.0 // indirect
@@ -13,5 +18,4 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/image v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
-	gonum.org/v1/plot v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"os"
 	"slices"
 	"sync"
 	"time"
 
+	"gonum.org/v1/gonum/stat"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
 
-const STEP = 5
-const MAX = 10_000
+const STEP = 20
+const MAX = 8_000
 const TESTS_PER = 100_000
 const LOG_RESULTS = false
+const PLOT = false
+const REGRESSION = false
 
 type TrialResults struct {
 	avg float64
@@ -49,25 +53,65 @@ func main() {
 		}
 		return 1
 	})
+	fmt.Printf("Took %v to run %d trials, each with %d tests.\n", time.Since(start), MAX/STEP, TESTS_PER)
 
-	p := plot.New()
-	p.Title.Text = "Random Maximum vs. Average Number of Loops"
-	p.X.Label.Text = "Maximum Random Number"
-	p.Y.Label.Text = "Average Number of Loops"
-	pts := make(plotter.XYs, len(resultsSlice))
-	for i, res := range resultsSlice {
-		pts[i].X = float64(res.x)
-		pts[i].Y = res.avg
+	for _, res := range resultsSlice {
+		fmt.Printf("%f, ", float64(res.x))
 	}
-	err := plotutil.AddLinePoints(p, "Points", pts)
-	if err != nil {
-		fmt.Printf("Could not add line points: %v\n", err)
-		os.Exit(1)
+	fmt.Print("\n\n")
+	for _, res := range resultsSlice {
+		fmt.Printf("%f, ", res.avg)
+	}
+	fmt.Print("\n\n")
+
+	if PLOT {
+		// Plot
+		fmt.Println("Plotting...")
+		p := plot.New()
+		p.Title.Text = "Random Maximum vs. Average Number of Loops"
+		p.X.Label.Text = "Maximum Random Number"
+		p.Y.Label.Text = "Average Number of Loops"
+		pts := make(plotter.XYs, len(resultsSlice))
+		for i, res := range resultsSlice {
+			pts[i].X = float64(res.x)
+			pts[i].Y = res.avg
+		}
+		err := plotutil.AddLinePoints(p, "Points", pts)
+		if err != nil {
+			fmt.Printf("Could not add line points: %v\n", err)
+			os.Exit(1)
+		}
+
+		if err = p.Save(4*vg.Inch, 4*vg.Inch, "results.png"); err != nil {
+			fmt.Println("Error saving plot to file: ", err)
+			os.Exit(1)
+		}
 	}
 
-	if err = p.Save(4*vg.Inch, 4*vg.Inch, "results.png"); err != nil {
-		fmt.Println("Error saving plot to file: ", err)
-		os.Exit(1)
+	if REGRESSION {
+		// Regression
+		fmt.Println("Running regression...")
+		xVals := make([]float64, len(resultsSlice))
+		yVals := make([]float64, len(resultsSlice))
+		logXVals := make([]float64, len(resultsSlice))
+		logYVals := make([]float64, len(resultsSlice))
+		for i, res := range resultsSlice {
+			if res.avg < 0.0 {
+				fmt.Printf("error: cannot perform regression on negative y value (avg) for result %+v\n", res)
+				os.Exit(1)
+			}
+			xVals[i] = float64(res.x)
+			yVals[i] = res.avg
+			logXVals[i] = math.Log(float64(res.x))
+			logYVals[i] = math.Log(res.avg)
+		}
+
+		aLog, bLog := stat.LinearRegression(logXVals, yVals, nil, false)
+		aExp, bExp := stat.LinearRegression(xVals, logYVals, nil, false)
+		aExp = math.Exp(aExp)
+
+		fmt.Printf("Log model: y = %f * log(x) + %f\n", aLog, bLog)
+		fmt.Printf("Exp model: y = %f * exp(%f*x)\n", aExp, bExp)
 	}
 
 	if LOG_RESULTS {
@@ -76,7 +120,7 @@ func main() {
 			fmt.Printf("%8d: %-8f\n", res.x, res.avg)
 		}
 	}
-	fmt.Printf("Took %v to run %d trials, each with %d tests.\n", time.Since(start), MAX/STEP, TESTS_PER)
+	fmt.Printf("Took %v to finish.\n", time.Since(start))
 }
 
 func trialFor(x int, results chan TrialResults, trialGroup *sync.WaitGroup) {
